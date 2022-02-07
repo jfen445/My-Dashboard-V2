a06d80adc4892886ba5cad152ee1d90aadf45fa4
jfen445 <jfen445@aucklanduni.ac.nz>
2022-02-07 17:37:45 +1300

    add stocks page

---
+++ pages/stocks.js
@@ -0,0 +1,13 @@
+import Table from "../components/Stocks/Table";
+
+function StocksPage() {
+  return (
+    <>
+      <div className="fpy-4 sm:px-6 md:px-8">
+        <Table />
+      </div>
+    </>
+  );
+}
+
+export default StocksPage;
