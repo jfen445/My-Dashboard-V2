ba5647f95c86e664c1e26479c4908de047198414
jfen445 <jfen445@aucklanduni.ac.nz>
2022-02-07 18:25:48 +1300

    Add music page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary-blue": {
+                    100: "#259FDE",
+                    50: "#259BDD",
+                    15: "#E4EDF8",
+                    10: "#ECF2F9",
+                },
+            },
+        },
     },
     plugins: [],
 };
